21d4e6b87a388073ac41912622f7a15c005812eb
Mahesh Kapoor <nov17megha@yahoo.co.in>
2018-09-29 10:16:14 +1000

    first commit

---
+++ src/app/shared/quiz.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class QuizService {
+  //---------------- Properties---------------
+  readonly rootUrl = 'http://localhost:8080';
+  qns: any[];
+  seconds: number;
+  timer;
+  qnProgress: number;
+  correctAnswerCount: number = 0;
+
+  //---------------- Helper Methods---------------
+  constructor(private http: HttpClient) { }
+  displayTimeElapsed() {
+    return Math.floor(this.seconds / 3600) + ':' + Math.floor(this.seconds / 60) + ':' + Math.floor(this.seconds % 60);
+  }
+
+  getParticipantName() {
+    var participant = JSON.parse(localStorage.getItem('participant'));
+    return participant.Name;
+  }
+
+
+  //---------------- Http Methods---------------
+
+  insertParticipant(name: string, email: string) {
+    var user = [name, email]
+    return this.http.post(this.rootUrl + '/validateAccount', user);
+  }
+
+  getQuestions() {
+    return this.http.get(this.rootUrl + '/questions');
+  }
+
+  getAnswers() {
+    var body = this.qns.map(x => x.QnID);
+    return this.http.post(this.rootUrl + '/answers', body);
+  }
+
+  submitScore() {
+    var body = JSON.parse(localStorage.getItem('participant'));
+    body.Score = this.correctAnswerCount;
+    body.TimeSpent = this.seconds;
+    return this.http.post(this.rootUrl + "/api/UpdateOutput", body);
+  }
+
+}
