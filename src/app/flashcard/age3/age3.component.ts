df1d33479fb9970ff8b63ed9b5a05721dad35ea8
Mahesh Kapoor <nov17megha@yahoo.co.in>
2021-09-08 22:32:59 +1000

    flashcard - fixes and plain content

--- src/app/flashcard/age3/age3.component.ts
+++ src/app/flashcard/age3/age3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Title, Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-age3',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Age3Component implements OnInit {
 
-  constructor() { }
+  constructor(public meta: Meta, public title: Title) { }
 
   ngOnInit() {
     //document.getElementById("seo-main-text").style.display = "none";
+    this.updateMetaTags();
+  }
+
+  updateMetaTags(){
+    this.title.setTitle("Speech development Age 3");
+    this.meta.updateTag(  {name: "description", content: "Digital flashcards for speech development, speech improvement and speech therapy. Digital flashcards for kids at age 3."});
+    this.meta.updateTag(  {name : "keywords", content: "Digital Flashcards,  alphabets flashcard, numbers flashcard, fruits flashcard, vegetables flashcard, weathers flashcard, shapes flashcard, colors flashcard, transportation flashcard, animals flashcard, gestures flashcard, clothes flashcard, food flashcard, family flashcard, birds flashcard, plants flashcard"});
+    this.meta.updateTag(  {property : "og:url", content: "https://practisepoint.com/age3speechdevelopment"});
+    this.meta.updateTag(  {property : "og:image", content: "https://practisepoint.com/assets/img/age3speechbanner.png"});
+    this.meta.updateTag(  {property : "og:description", content: "Digital flashcards for speech development, speech improvement and speech therapy. Digital flashcards for kids at age 3."});
+    this.meta.updateTag(  {property : "og:title", content: "Speech development Age 3"});
   }
 
 }
+
