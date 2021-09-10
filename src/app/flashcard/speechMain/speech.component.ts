c9627ba8d0dd086e106cf78f4950b556576df47f
Mahesh Kapoor <nov17megha@yahoo.co.in>
2021-09-10 22:50:15 +1000

    seo in speech main page

--- src/app/flashcard/speechMain/speech.component.ts
+++ src/app/flashcard/speechMain/speech.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Inject} from '@angular/core';
 import {DOCUMENT} from "@angular/common";
+import {Title, Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-speech',
@@ -8,10 +9,21 @@ import {DOCUMENT} from "@angular/common";
 })
 export class SpeechComponent implements OnInit {
 
-  constructor(@Inject(DOCUMENT) private document: Document) { }
+  constructor(@Inject(DOCUMENT) private document: Document, public meta: Meta, public title: Title) { }
 
   ngOnInit() {
     //document.getElementById("seo-main-text").style.display = "none";
+    this.updateMetaTags();
+  }
+
+  updateMetaTags(){
+    this.title.setTitle("Speech development in kids");
+    this.meta.updateTag(  {name: "description", content: "Speech development, speech improvement and speech therapy for kids age 1, 2, 3, 4, 5.. Digital flash cards to improve english vocabulary"});
+    this.meta.updateTag(  {name : "keywords", content: "Speech development, speech improvement, speech therapy, age 1, age 2, age 3, age 4, age 5, Digital Flashcards,  alphabets flashcard, numbers flashcard, fruits flashcard, vegetables flashcard, weathers flashcard, shapes flashcard, colors flashcard, transportation flashcard, animals flashcard, gestures flashcard, clothes flashcard, food flashcard, family flashcard, birds flashcard, plants flashcard"});
+    this.meta.updateTag(  {property : "og:url", content: "https://practisepoint.com/speechdevelopment"});
+    this.meta.updateTag(  {property : "og:image", content: "https://practisepoint.com/assets/img/speechdevelopmentbanner.jpg"});
+    this.meta.updateTag(  {property : "og:description", content: "Speech development, speech improvement and speech therapy for kids age 1, 2, 3, 4, 5. Digital flash cards to improve english vocabulary"});
+    this.meta.updateTag(  {property : "og:title", content: "Speech development in kids"});
   }
 
 }
